f70ddfab82793c695f927994472bf255c283f09a
Young hoon kim <ykim2@atlassian.com>
2023-02-01 21:03:14 +1300

    Initial Commit

---
+++ 100hr/_00hrApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00hrApp.swift
+//  100hr
+//
+//  Created by young hoon kim on 1/02/23.
+//
+
+import SwiftUI
+
+@main
+struct _00hrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
